23c6f31a07ea53702cc5e47d7be8c4615d7032f3
salazenas <salazenas@gmail.com>
2020-08-25 01:13:39 -0300

    heroku settings and fixes

--- src/config/server.js
+++ src/config/server.js
@@ -1,18 +1,18 @@
-const port = 3003
+const port = process.env.PORT || 3003;
 
-const bodyParser = require('body-parser')
-const express = require('express')
-const server = express()
-const cors = require('../config/cors')
-const queryParser = require('express-query-int')
+const bodyParser = require("body-parser");
+const express = require("express");
+const server = express();
+const cors = require("../config/cors");
+const queryParser = require("express-query-int");
 
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
-server.use(cors)
-server.use(queryParser())
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(cors);
+server.use(queryParser());
 
 server.listen(port, () => {
-    console.log(`BACKEND running on port ${port}.`)
-})
+    console.log(`BACKEND running on port ${port}.`);
+});
 
-module.exports = server
+module.exports = server;
